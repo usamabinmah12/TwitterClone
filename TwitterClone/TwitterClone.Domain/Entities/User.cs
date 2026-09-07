dd692003caa91b833c1302f27c97867d925ba15f
usamabinmah12 <usamabinmahbub13@gmail.com>
2026-09-07 19:37:34 +0600

    User Class Implemented

---
+++ TwitterClone/TwitterClone.Domain/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿namespace TwitterClone.Domain.Entities
+{
+    internal class User
+    {
+        private Guid _id;
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        public User()
+        {
+            _id = Guid.NewGuid();
+
+        }
+        public Guid Id
+        {
+            get { return _id; }
+        }
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+    }
+}
