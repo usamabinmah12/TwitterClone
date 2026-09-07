0e2b9a3d94f8008852130413565b098438e995a0
usamabinmah12 <usamabinmahbub13@gmail.com>
2026-09-07 20:10:13 +0600

    Message for Sender and Receiver added

---
+++ TwitterClone/TwitterClone.Domain/Entities/Message.cs
@@ -0,0 +1,46 @@
+﻿
+
+namespace TwitterClone.Domain.Entities
+{
+    public class Message
+    {
+        private Guid _id;
+        private Guid _senderId;
+        private Guid _receiverId;
+        private string _content;
+        private DateTime _sentAt;
+        private bool _isRead;
+
+        public Guid Id
+        {
+            get { return _id; }
+        }
+
+        public Guid SenderId
+        {
+            get { return _senderId; }
+        }
+
+        public Guid ReceiverId
+        {
+            get { return _receiverId; }
+        }
+
+        public string Content
+        {
+            get { return _content; }
+            set { _content = value; }
+        }
+
+        public DateTime SentAt
+        {
+            get { return _sentAt; }
+        }
+
+        public bool IsRead
+        {
+            get { return _isRead; }
+            set { _isRead = value; }
+        }
+    }
+}
